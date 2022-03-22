b7fc9f71370060bf25ad9f98aafea863bd0fe858
Nesh Patel <nesh.patel1@gmail.com>
2022-03-22 09:04:14 +0000

    Updated game to handle repeated characters.

--- tests/test_wordle.py
+++ tests/test_wordle.py
@@ -1,5 +1,5 @@
 import unittest
-from wordle import Word, WrongWordSize, WORD_SIZE
+from wordle import Game, Word, WrongWordSize, WORD_SIZE
 
 
 class Words(unittest.TestCase):
@@ -13,6 +13,39 @@ class Words(unittest.TestCase):
         self.assertFalse(word.has_unique_letters)
         word = Word('crane')
         self.assertTrue(word.has_unique_letters)
+
+    def test_letter_counts(self):
+        word = Word('ghost')
+        self.assertEqual(word.letter_map, {'G': 1, 'H': 1, 'O': 1, 'S': 1, 'T': 1})
+        self.assertEqual(word.repeated_letters, [])
+
+        word = Word('renew')
+        self.assertEqual(word.letter_map, {'R': 1, 'E': 2, 'N': 1, 'W': 1})
+        self.assertEqual(word.repeated_letters, ['E'])
+
+    def test_guesses(self):
+        game = Game('ghost')
+        game.guess('pious')
+        self.assertEqual(str(game.last_guess), '⬛⬛🟩⬛🟨')
+        game.guess('slosh')
+        self.assertEqual(str(game.last_guess), '⬛⬛🟩🟩⬛')
+        game.guess('ghost')
+        self.assertEqual(str(game.last_guess), '🟩🟩🟩🟩🟩')
+
+    def test_repeated_letters(self):
+        game = Game('where')
+        game.guess('there')
+        self.assertEqual(str(game.last_guess), '⬛🟩🟩🟩🟩')
+        game.guess('keeps')
+        self.assertEqual(str(game.last_guess), '⬛🟨🟩⬛⬛')
+        game.guess('apple')
+        self.assertEqual(str(game.last_guess), '⬛⬛⬛⬛🟩')
+        game.guess('abbey')
+        self.assertEqual(str(game.last_guess), '⬛⬛⬛🟨⬛')
+
+        game = Game('abbey')
+        game.guess('keeps')
+        self.assertEqual(str(game.last_guess), '⬛🟨⬛⬛⬛')
 
 
 if __name__ == '__main__':
--- wordle.py
+++ wordle.py
@@ -1,13 +1,14 @@
 import re
+import os
 from random import randrange
 from enum import Enum
 from utils import log
-from typing import List, Optional
+from typing import Optional, Union
 
 
 WORD_SIZE = 5
 NUM_GUESSES = 6
-WORD_LIST = 'word_list.txt'
+WORD_LIST = os.path.join(os.path.dirname(__file__), 'word_list.txt')
 
 
 class WrongWordSize(ValueError):
@@ -38,6 +39,18 @@ class Word:
     def has_unique_letters(self):
         return len(set(self.word)) == len(self.word)
 
+    @property
+    def letter_map(self) -> dict[str, int]:
+        out = {}
+        for char in self.word:
+            out[char] = out.get(char, 0)
+            out[char] += 1
+        return out
+
+    @property
+    def repeated_letters(self) -> list[str]:
+        return [k for k, v in self.letter_map.items() if v > 1]
+
     def __str__(self):
         return self.word
 
@@ -51,14 +64,19 @@ class Letter:
         guess: str,
         position: int,
         answer: Word,
+        override: Optional[GuessState] = None,
     ):
         assert len(guess) == 1, "guess and answer must be single letters."
         self.guess = guess.upper()
         self.position = position
         self.answer = answer
+        self.override = override
 
     @property
     def state(self) -> GuessState:
+        if self.override:
+            return self.override
+
         if self.guess == self.answer.word[self.position]:
             return GuessState.CORRECT
         elif self.guess in self.answer.word:
@@ -66,18 +84,29 @@ class Letter:
         else:
             return GuessState.WRONG
 
+    def __str__(self) -> str:
+        return self.guess.upper()
+
+
+class Guess(list):
+    def __init__(self, *args, **kwargs):
+        super(Guess, self).__init__(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return ''.join(letter.state.value for letter in self)
+
 
 class Game:
     def __init__(
         self,
-        answer: Word,
-        word_list: Optional[List[Word]] = None
+        answer: Union[str, Word],
+        word_list: Optional[list[Word]] = None
     ):
-        self.answer: Word = answer
+        self.answer: Word = Word(answer) if isinstance(answer, str) else answer
         self.num_guesses: int = NUM_GUESSES
         self.turn: int = 0
-        self.guesses: List[List[Letter]] = []
-        self.word_list: List[Word] = word_list or load_words()
+        self.guesses: list[Guess] = []
+        self.word_list: list[Word] = word_list or load_words()
 
     @property
     def is_over(self) -> bool:
@@ -94,7 +123,11 @@ class Game:
     def score(self) -> int:
         return len(self.guesses)
 
-    def make_guess(self, _guess: str) -> bool:
+    @property
+    def last_guess(self) -> Guess:
+        return self.guesses[-1]
+
+    def guess(self, _guess: str) -> bool:
         if self.is_over:
             return False  # No more guesses remaining
 
@@ -108,9 +141,32 @@ class Game:
             log.warning(f"{guess} is not a valid word, please try again.")
             return False  # Not a valid word, guess again
 
-        guess_states = []
+        guess_states = Guess()
         for i, letter in enumerate(guess.word):
             guess_states.append(Letter(letter, i, self.answer))
+
+        # Handle repeated letter behaviour:
+        # https://nerdschalk.com/wordle-same-letter-twice-rules-explained-how-does-it-work/
+        if guess.repeated_letters:
+            for repeated_letter in guess.repeated_letters:
+                if repeated_letter not in self.answer.word:
+                    continue
+
+                # Get the total occurrences of the letter in the answer
+                num_in_answer = self.answer.letter_map[repeated_letter]
+                num_matched = 0
+
+                # Count all the exact matches first
+                for letter in guess_states:
+                    if letter.state == GuessState.CORRECT:
+                        num_matched += 1
+
+                # For remaining occurrences, ensure only the number of occurrences in the answer are indicated.
+                for letter in guess_states:
+                    if letter.state == GuessState.POSITION:
+                        num_matched += 1
+                        if num_matched > num_in_answer:
+                            letter.override = GuessState.WRONG
 
         self.guesses.append(guess_states)
         return True
@@ -118,14 +174,14 @@ class Game:
     def __str__(self):
         out = '\n'
         for guess in self.guesses:
-            out += ''.join(letter.guess for letter in guess)
+            out += ''.join(str(letter) for letter in guess)
             out += '\n'
-            out += ''.join(letter.state.value for letter in guess)
+            out += str(guess)
             out += '\n'
         return out
 
 
-def load_words() -> List[Word]:
+def load_words() -> list[Word]:
     word_list = []
     with open(WORD_LIST, 'r') as f:
         for line in f.readlines():
@@ -143,15 +199,14 @@ def play_game():
     while not game.is_over:
         print(f'Guess {len(game.guesses) + 1}:')
         guess = input()
-        if game.make_guess(guess):
+        if game.guess(guess):
             print(game)
     print()
     print(f"Answer: {game.answer}")
 
 
-
 def main():
-    pass
+    play_game()
 
 
 if __name__ == '__main__':
